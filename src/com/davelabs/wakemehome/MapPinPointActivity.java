1bdb16545b2e5876ccdde0b7a0a266edb1e1653c
Dave Aitken <dave.aitken@gmail.com>
2012-12-21 13:38:55 +0000

    Started java view wrapper code

---
+++ src/com/davelabs/wakemehome/MapPinPointActivity.java
@@ -0,0 +1,20 @@
+package com.davelabs.wakemehome;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+
+public class MapPinPointActivity extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_pinpoint);
+    }
+}
