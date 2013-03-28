297977b9ecdee1bd68ca7bd8c1123db7be766289
David Chadwick <david.chadwick7@gmail.com>
2013-03-28 14:53:40 +0000

    added uncommited Alarm activity

---
+++ wakemehome/src/com/davelabs/wakemehome/AlarmActivity.java
@@ -0,0 +1,14 @@
+package com.davelabs.wakemehome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmActivity extends Activity{
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm);
+	}
+
+}
